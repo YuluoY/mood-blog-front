e31472fc7bfd15b43135833ba166ad7a42d81286
yuluo <568055454@qq.com>
2023-11-25 14:42:31 +0800

    feat: 添加stylelint校验

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 import eslintPlugin from 'vite-plugin-eslint'
+import stylelintPlugin from 'vite-plugin-stylelint'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), eslintPlugin()],
+  plugins: [vue(), eslintPlugin(), stylelintPlugin({ fix: true })],
   server: {
     host: '127.0.0.1',
     port: 8080,
